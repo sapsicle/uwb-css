6a951b113a69ea852d2cbcd71a8aa6723abfaa41
andrew <n.coop912@gmail.com>
2022-11-15 06:06:27 -0800

    lab method solutions

--- css skl 142/Lab7.java
+++ css skl 142/Lab7.java
@@ -16,18 +16,25 @@ public class Lab7 {
 
     // Print a copy of str to standard output, with the
     // first letter of each word in upper case
-    static void capitalizeFirstLetters( String str ) {
-        // String[] sentence = str.split(" ", 0);
-        Scanner in = new Scanner(str);
-        String word;
-        do {
-            word = in.next();
+    static void capitalizeFirstLetters(String str) {
+        String[] sentence = str.split(" ");
+        for (String word : sentence) {
             Character.toUpperCase(word.charAt(0));
             System.out.print(word + " ");
-        } while (in.hasNext());
-        System.out.println(); //newline
-        // Your logic goes here
-        in.close();
+        }
+        System.out.println();
+        
+        // String[] sentence = str.split(" ", 0);
+        // Scanner in = new Scanner(str);
+        // String word;
+        // do {
+        //     word = in.next();
+        //     Character.toUpperCase(word.charAt(0));
+        //     System.out.print(word + " ");
+        // } while (in.hasNext());
+        // System.out.println(); //newline
+        // // Your logic goes here
+        // in.close();
     }
 
 }  // end class
