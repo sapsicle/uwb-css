0a964d1a53591187d26d46d5b43038ecd86b82b5
andrew <n.coop912@gmail.com>
2023-01-09 19:25:08 -0800

    euclidian algorithm

--- css 143/FractionsV1.java
+++ css 143/FractionsV1.java
@@ -4,18 +4,12 @@ public class FractionsV1 {
 
     private static int usedIndices;
     private static int[][] fractions;
-    private static int numerator = 0, denominator = 0;
+    // private static int numerator = 0, denominator = 0;
 
     public static void main(String[] args) {
-        Scanner filein = null;
-        try {
-            filein = new Scanner(new FileInputStream("fractions.txt"));
-        } catch (IOException e) {
-            System.err.println("File not found");
-            System.exit(0);
-        }
+        Scanner filein = makeFileScanner();
         
-        createArray(countFractions(filein));
+        createArray(countFractions());
         System.out.println("array length: " + fractions.length);
 
         do {
@@ -23,16 +17,7 @@ public class FractionsV1 {
             addFraction(fraction);
         } while (filein.hasNextLine());
 
-        // System.out.println("number of fractions: " + usedIndices);
-        // System.out.println("fraction #17: " + fractions[16][0] + "/" + fractions[16][1]);
 
-
-        
-
-
-
-
-        // System.out.println("numerator: " + num + ", denominator: " + den);
 
 
 
@@ -48,14 +33,27 @@ public class FractionsV1 {
         fractions = new int[indices][2];
     }
 
-    public static int countFractions(Scanner sc) {
+    public static int countFractions() {
+        Scanner sc = makeFileScanner();
         int indices = 0;
         do {
             indices++;
+            sc.nextLine();
         } while (sc.hasNextLine());
+        sc.close();
         return indices;
     }
 
+    public static Scanner makeFileScanner() {
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new FileInputStream("fractions.txt"));
+        } catch (IOException e) {
+            System.err.println("File not found");
+            System.exit(0);
+        }
+        return sc;
+    }
 
     public static void addFraction(String fraction) {
         String[] arr = fraction.split("/");
@@ -68,5 +66,33 @@ public class FractionsV1 {
             fractions[usedIndices][1] = Integer.parseInt(arr[1]);
         }
         usedIndices++;
+    }
+
+    /**
+     * PRE: N/A
+     * @param numerator
+     * @param denominator
+     * @return Greatest Common Denominator between two numbers
+     */
+    public static int euclidianAlgorithm(int numerator, int denominator) {
+        int remainder = 0, GCD = 1;
+
+        // switch if numerator less than denominator, as a needs to be larger than b for algorithm
+        if (numerator < denominator) {
+            int temp = numerator;
+            numerator = denominator;
+            denominator = temp;
+        }
+
+        do {
+            remainder = numerator % denominator; // r = a % b
+            numerator = denominator; // replace a with b
+            denominator = remainder; // replace b with r
+        } while (remainder != 0);
+        
+        GCD = numerator; // GCD would be the last denominator in sequence, which is now stored in numerator
+        // could've kept it in denominator but would've needed unnecessary break
+
+        return GCD;
     }
 }
