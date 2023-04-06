7950b7c76ad436246795c928700bd926b1b3b3c6
andrew <n.coop912@gmail.com>
2023-04-06 05:40:01 -0700

    assignment 1 bulk workload
    Former-commit-id: 2648e85668789cfcf52c26f5764956f38b043554

--- css 342/assignment 1/library.cpp
+++ css 342/assignment 1/library.cpp
@@ -11,38 +11,70 @@
 
 // constructor with default name
 Library::Library(const string &Name) {
-
+    // static string library[MAX];
+    this->numBooks = 0;
 }
 
 // destructor
 // nothing on heap
 Library::~Library() {
-  // destructor
+    // destructor
 }
 
 // true if book found in library
 bool Library::isInLibrary(const string &BookName) const {
-  return true;
+    if (this->numBooks == 0) return false;
+    for (int i{0}; i < this->numBooks; i++) {return (BookName == this->library[i]) ? true : false;}
+    // dummy return, compiler unhappy
+    return false;
 }
 
 // add a new book
 // return true if successful, false if
 // book already in library
 bool Library::addBook(const string &BookName) {
-  return false;
+    // if book already exists, return early with false
+    if (isInLibrary(BookName)) return false;
+
+    // set book to numBooks index
+    // numBooks == 0 will place it in library[0] which is the first index
+    // numBooks == 5 will place it in library[5] which is the 6th index
+    library[this->numBooks] = BookName;
+    // then increment to account for new book
+    this->numBooks++;
+
+    // if successful return true (got here without error)
+    return true;
 }
 
 // remove a book
 // return true if successfully removed
 // false if book not in library
 bool Library::removeBook(const string &BookName) {
-  return false;
+    // if target book not in library, return early with false
+    if (!isInLibrary(BookName)) {return false;}
+
+    // loop through library array up to number of books (filled size)
+    for (int i{0}; i < this->numBooks; i++) {
+        // check for match with each book
+        if (this->library[i] == BookName) {
+            // if matched, move last position book to fill index of book to remove
+            this->library[i] = this->library[this->numBooks - 1];
+            // decrement to account for lost book
+            this->numBooks--;
+        }
+    }
+
+    // if no errors return true
+    return true;
 }
 
 // list all books
 void Library::listAllBooks() const {
+    // loop through filled portion of library and output all books on individual lines
+    for (int i{0}; i < this->numBooks; i++) {cout << this->library[i] << endl;}
 }
 
 ostream &operator<<(ostream &Out, const Library &Lib) {
-  return Out;
+    return Out;
 }
--- css 342/assignment 1/library.h
+++ css 342/assignment 1/library.h
@@ -11,35 +11,38 @@
 using namespace std;
 
 // Library holds books - can add, remove and list books
-class Library {
-  // display all books in library
-  friend ostream &operator<<(ostream &Out, const Library &Lib);
+class Library
+{
+    // display all books in library
+    friend ostream &operator<<(ostream &Out, const Library &Lib);
 
 public:
-  // constructor with default name
-  explicit Library(const string &Name);
+    // constructor with default name
+    explicit Library(const string &Name);
 
-  // destructor
-  virtual ~Library();
+    // destructor
+    virtual ~Library();
 
-  // add a new book
-  // return true if successful, false if
-  // book already in library
-  bool addBook(const string &BookName);
+    // add a new book
+    // return true if successful, false if
+    // book already in library
+    bool addBook(const string &BookName);
 
-  // remove a book
-  // return true if successfully removed
-  // false if book not in library
-  bool removeBook(const string &BookName);
+    // remove a book
+    // return true if successfully removed
+    // false if book not in library
+    bool removeBook(const string &BookName);
 
-  // list all books
-  void listAllBooks() const;
+    // list all books
+    void listAllBooks() const;
 
-  // true if book found in library
-  bool isInLibrary(const string &BookName) const;
+    // true if book found in library
+    bool isInLibrary(const string &BookName) const;
 
 private:
-
+    static const int MAX = 100;
+    static string library[MAX];
+    static int numBooks;
 };
 
-#endif  // ASS1_LIBRARY_H
+#endif // ASS1_LIBRARY_H
