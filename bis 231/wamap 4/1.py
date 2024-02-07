bdcbaf6555f64377b99dd5eda69444656d96301b
andrew <n.coop912@gmail.com>
2024-02-06 16:41:05 -0800

    wamap 4 1-5

---
+++ bis 231/wamap 4/1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+A = np.array([
+    [3, -2, 4],
+    [1, -5, -8]
+])
+
+print(A)
+print(A.T)
---
+++ bis 231/wamap 4/5.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+A = np.array([
+    [6, -1],
+    [3, 1],
+    [4, -2]
+])
+
+print(np.dot(A.T, A))
